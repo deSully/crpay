a146edc83eccccd007bb8e3ff6a51a26e16a95da
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-06-08 12:43:16 +0200

    add transaction detail view

--- transaction/urls.py
+++ transaction/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views.intouch_callback import InTouchCallbackView
 from .views.transaction import TransactionView
+from .views.transaction_detail import TransactionDetailView
 
 urlpatterns = [
     path("", TransactionView.as_view(), name="create_transaction"),
+    path("transactions/<uuid:uuid>/", TransactionDetailView.as_view(), name="transaction-detail"),
     path("callback", InTouchCallbackView.as_view(), name="intouch-callback"),
 ]
---
+++ transaction/views/transaction_detail.py
@@ -0,0 +1,31 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from transaction.models import Transaction
+from transaction.serializers import TransactionSerializer
+
+
+class TransactionDetailView(RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    lookup_field = "uuid"
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.entity_type == "INTERNAL":
+            return Transaction.objects.all()
+        return Transaction.objects.filter(entity=user)
+
+    @swagger_auto_schema(
+        operation_summary="Détails d'une transaction",
+        operation_description="Permet de consulter l'état actuel d'une transaction grâce à son UUID. Utile pour vérifier l'état après un traitement asynchrone ou un callback.",
+        responses={
+            200: TransactionSerializer(),
+            404: "Transaction non trouvée ou non autorisée.",
+        },
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
